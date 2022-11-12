2fe85cd67e0c141cd3c40aa62d69af122aaba49d
Wester <wester311@gmail.com>
2022-11-12 23:03:19 +0700

    [fix] comments deleted from tags content

--- src/crawler.py
+++ src/crawler.py
@@ -11,7 +11,7 @@ from typing import List
 import aiohttp
 import bs4
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 from loguru import logger
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -258,6 +258,9 @@ class ParseUtils:
         soup = BeautifulSoup(text, "html.parser")
         for data in soup(["style", "script", "meta", "template"]):
             data.decompose()
+
+        for element in soup(text=lambda text: isinstance(text, Comment)):
+            element.extract()
 
         return OmegaParser3000.merge_text_and_links(soup.find_all(), base_url)
 
